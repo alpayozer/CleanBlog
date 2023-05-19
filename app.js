c26dcb6cda97e16e046b8a0eddf5cde9fdf420f4
Alpay Özer <alpayozer7@gmail.com>
2023-05-19 18:20:59 +0300

    ödev8 tamamlandı

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
 
+//template engine
+app.set("view engine","ejs");
+
+//middleware
+app.use(express.static("public"));
+
+
 app.get("/",(req,res)=>{
-
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description"
-    }
-
-    res.send(blog);
-
+    res.render("index")
 })
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+app.get("/add_post",(req,res)=>{
+    res.render("add_post")
+})
+app.get("/post",(req,res)=>{
+    res.render("post")
+})
+
 
 const port = 3000;
 
