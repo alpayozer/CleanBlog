6503d3b2ca413d8610483ae73a36553b61fe5262
Alpay Özer <alpayozer7@gmail.com>
2023-05-22 16:58:17 +0300

    ödev 9 tamamlandı

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const ejs = require("ejs");
+const Blog = require("./models/Blog");
 
 const app = express();
+
+//connect db
+mongoose.connect("mongodb://localhost/myblog",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
 
 //template engine
 app.set("view engine","ejs");
 
 //middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-app.get("/",(req,res)=>{
-    res.render("index")
+app.get("/",async(req,res)=>{
+    const blogs = await Blog.find({});
+    res.render("index",{
+        blogs
+    })
 })
 app.get("/about",(req,res)=>{
     res.render("about")
@@ -21,6 +34,10 @@ app.get("/add_post",(req,res)=>{
 })
 app.get("/post",(req,res)=>{
     res.render("post")
+})
+app.post("/blogs",async(req,res)=>{
+    await Blog.create(req.body);
+    res.redirect("/");
 })
 
 
